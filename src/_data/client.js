9b15e89f81cdd80dcaa3a23e3f6d5d2332c88b34
“andrewbramwell <andy@voucherstore.io>
2025-12-15 16:24:42 +0700

    schema

--- src/_data/client.js
+++ src/_data/client.js
@@ -20,7 +20,52 @@ module.exports = {
         "googleReview": "",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://patthirathaispa.co.uk"
+    "domain": "https://www.patthirathaispa.co.uk",
+    "schema": {
+        "business": {
+            "@context": "https://schema.org",
+            "@type": "HealthAndBeautyBusiness",
+            "@id": "https://www.patthirathaispa.co.uk/#business",
+            "name": "Patthira Thai Spa",
+            "image": "https://www.patthirathaispa.co.uk/assets/svgs/logo.svg",
+            "url": "https://www.patthirathaispa.co.uk",
+            "telephone": "+441604946448",
+            "email": "patthirathaispa@gmail.com",
+            "priceRange": "££",
+            "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "34 York Rd",
+                "addressLocality": "Northampton",
+                "postalCode": "NN1 5QH",
+                "addressCountry": "GB"
+            },
+            "geo": {
+                "@type": "GeoCoordinates",
+                "url": "https://maps.app.goo.gl/oNukY9Las2WTSCdT8"
+            },
+            "openingHoursSpecification": [
+                {
+                    "@type": "OpeningHoursSpecification",
+                    "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+                    "opens": "10:00",
+                    "closes": "20:00"
+                }
+            ],
+            "sameAs": [
+                "https://www.facebook.com/profile.php?id=61572085995736"
+            ]
+        },
+        "website": {
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "@id": "https://www.patthirathaispa.co.uk/#website",
+            "url": "https://www.patthirathaispa.co.uk",
+            "name": "Patthira Thai Spa",
+            "publisher": {
+                "@id": "https://www.patthirathaispa.co.uk/#business"
+            }
+        }
+    }
 };
 
 
